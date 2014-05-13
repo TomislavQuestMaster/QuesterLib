038327ee9788e57419c80e461b3c77523f0245c4
tdubravcevic <Tomislav.Dubravcevic@infobip.com>
2014-05-13 11:20:41 +0200

    implementing tests

--- src/test/java/net/thequester/processor/GameEngineTest.java
+++ src/test/java/net/thequester/processor/GameEngineTest.java
@@ -10,6 +10,10 @@ import org.junit.Test;
 
 import java.util.HashMap;
 
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+import static org.mockito.Matchers.anyInt;
+import static org.mockito.Matchers.eq;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
 
@@ -19,10 +23,12 @@ import static org.mockito.Mockito.when;
 public class GameEngineTest {
 
 	private GameEngine engine;
-	private QuestProcessor questProcessor = mock(QuestProcessor.class);
-	private EventProcessor eventProcessor = mock(EventProcessor.class);
+	private final QuestProcessor questProcessor = mock(QuestProcessor.class);
+	private final EventProcessor eventProcessor = mock(EventProcessor.class);
 
-	QuestLocation location;
+	private QuestLocation location;
+	private Integer currentNode;
+
 
 	@Before
 	public void setUp() {
@@ -31,27 +37,66 @@ public class GameEngineTest {
 	}
 
 	@Test
-	public void test() {
+	public void happyPath() {
 
 		location = new QuestLocation(1.0, 1.0);
 
-		method();
-		method1();
+		givenCurrent(null);
+		givenNodeFromProcessor(1);
+		givenSuccessfulEvent();
+
+		whenEngineProcessesLocation();
+
+		thenIsInPath(1);
+	}
+
+	@Test
+	public void eventFails() {
+
+		location = new QuestLocation(1.0, 1.0);
+
+		givenCurrent(null);
+		givenNodeFromProcessor(1);
+		givenUnSuccessfulEvent();
+
+		whenEngineProcessesLocation();
+
+		thenIsNotInPath(1);
+	}
+
+	private void whenEngineProcessesLocation() {
 
 		engine.processLocation(location);
-
 	}
 
-	private void method() {
+	private void givenCurrent(Integer node){
+		currentNode = node;
+	}
 
+	private void givenNodeFromProcessor(Integer id){
 		Node node = new Node();
-		node.setId(2);
-
-		when(questProcessor.processLocation(null, location)).thenReturn(node);
+		node.setId(id);
+		when(questProcessor.processLocation(currentNode, location)).thenReturn(node);
 	}
-	private void method1() {
 
-		when(eventProcessor.isCauseFulfilled(2, new HashMap<Integer, Integer>())).thenReturn(true);
+	private void givenSuccessfulEvent() {
+
+		when(eventProcessor.isCauseFulfilled(anyInt(), eq(new HashMap<Integer, Integer>()))).thenReturn(true);
+	}
+
+	private void givenUnSuccessfulEvent() {
+
+		when(eventProcessor.isCauseFulfilled(anyInt(), eq(new HashMap<Integer, Integer>()))).thenReturn(false);
+	}
+
+	private void thenIsInPath(Integer node){
+
+		assertTrue(engine.getGamePath().contains(node));
+	}
+
+	private void thenIsNotInPath(Integer node){
+
+		assertFalse(engine.getGamePath().contains(node));
 	}
 
 }
